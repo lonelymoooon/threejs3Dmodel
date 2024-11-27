b34ca3f030df1765359a824c174a08e7e4500a70
lonelymoooon <128605546+lonelymoooon@users.noreply.github.com>
2024-11-27 21:57:38 +0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,80 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+// Сцена
+const scene = new THREE.Scene();
+
+// Отрисовщик
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.outputColorSpace = THREE.SRGBColorSpace;
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor(0x000000);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+document.body.appendChild(renderer.domElement);
+
+// Камера
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(10, 5, 10);
+
+// OrbitControls
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.enablePan = false;
+controls.minDistance = 5;
+controls.maxDistance = 20;
+controls.minPolarAngle = 0.5;
+controls.maxPolarAngle = 1.5;
+controls.autoRotate = false;
+controls.target = new THREE.Vector3(0, 1, 0);
+controls.update();
+
+// Освещение
+const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.25, 1);
+spotLight.position.set(0, 25, 0);
+spotLight.castShadow = true;
+spotLight.shadow.bias = -0.0001;
+scene.add(spotLight);
+
+// Пол
+const groundGeometry = new THREE.PlaneGeometry(20, 20, 30, 30);
+groundGeometry.rotateX(-Math.PI / 2);
+const groundMaterial = new THREE.MeshStandardMaterial({
+  color: 0x555555,
+  side: THREE.DoubleSide
+});
+const groundMesh = new THREE.Mesh(groundGeometry, groundMaterial);
+groundMesh.castShadow = false;
+groundMesh.receiveShadow = true;
+scene.add(groundMesh);
+
+// Матильда
+let matildaModel = null;
+
+const loader = new GLTFLoader().setPath('cat/');
+loader.load('scene.gltf', (gltf) => {
+  console.log('loading model');
+  matildaModel = gltf.scene;
+
+  matildaModel.traverse((child) => {
+    if (child.isMesh) {
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+
+  matildaModel.position.set(0, 0, 0);
+  matildaModel.scale.set(0.03, 0.03, 0.03);
+  scene.add(matildaModel);
+});
+
+// Анимация
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+
+animate();
